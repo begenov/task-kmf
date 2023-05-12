b1e08e3be6186f062a86520e93b9014ca2dac508
begenov <begenovorazali@icloud.com>
2023-05-12 07:24:07 +0600

    new sql repository

--- internal/repository/postgresql/postgresql.go
+++ internal/repository/postgresql/postgresql.go
@@ -1,1 +1,28 @@
 package postgresql
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/begenov/tesk-kmf/internal/model"
+)
+
+type PostgreSQL struct {
+	db *sql.DB
+}
+
+func NewDatabse(db *sql.DB) *PostgreSQL {
+	return &PostgreSQL{db: db}
+}
+
+func (db *PostgreSQL) CreateCurrency(ctx context.Context, currency model.Currency) error {
+	return nil
+}
+
+func (db *PostgreSQL) CurrencyByDateAndCode(ctx context.Context, date string, code string) ([]model.Currency, error) {
+	return nil, nil
+}
+
+func (db *PostgreSQL) CurrencyByDate(ctx context.Context, date string) ([]model.Currency, error) {
+	return nil, nil
+}
