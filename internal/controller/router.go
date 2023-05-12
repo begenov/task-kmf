693ead657f372c4fbf473b88176f2065b861a390
begenov <begenovorazali@icloud.com>
2023-05-12 11:56:03 +0600

    pix

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -9,8 +9,6 @@ import (
 func (c *Controller) InitRouter() *mux.Router {
 	mux := mux.NewRouter()
 
-	mux.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
-
 	mux.HandleFunc("/currency/save/{date}", c.saveCurrencyDate).Methods(http.MethodGet)
 	mux.HandleFunc("/currency/{date}", c.currencyHandler).Methods(http.MethodGet)
 	mux.HandleFunc("/currency/{date}/{code}", c.currencyHandler).Methods(http.MethodGet)
