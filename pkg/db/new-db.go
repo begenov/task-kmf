3293b60ec9aad3c3e834f83bab749dbc359ab506
begenov <begenovorazali@icloud.com>
2023-05-12 11:51:31 +0600

    pix

--- pkg/db/new-db.go
+++ pkg/db/new-db.go
@@ -2,16 +2,13 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
 func OpenDB(driver string, dsn string) (*sql.DB, error) {
-	fmt.Println(driver, dsn)
 	db, err := sql.Open(driver, dsn)
 	if err != nil {
-		fmt.Println("ok", err)
 		return nil, err
 	}
 	if err := db.Ping(); err != nil {
